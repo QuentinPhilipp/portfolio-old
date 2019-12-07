433f597ca5f04fc85f0b7d2c531c9447589624c9
Philipp Quentin <quentin.philipp@hotmail.fr>
2019-12-07 13:56:16 +0100

    Fix header,skills and spell check

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function goToHome() {
-  $('html, body').animate({
-    scrollTop: 0
-
-}, 1000);
-}
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return ( bounding.top <= document.documentElement.clientHeight);
+};
 
 var mybutton = document.getElementById("btnscroll");
+var myfooter = document.getElementById("footer");
 
 $(window).scroll(testScroll);
 
@@ -13,16 +12,39 @@ window.onload = testScroll()
 
 function testScroll() {
   var tempScrollTop = $(window).scrollTop();
-  console.log("Scroll from Top: " + tempScrollTop.toString());
+
+  // console.log(myfooter);
   if(tempScrollTop>500)
   {
     mybutton.style.display= 'block' ;
+
+    if (isInViewport(myfooter)) {
+        var bounding = myfooter.getBoundingClientRect();
+
+        var height = document.documentElement.clientHeight - bounding.top+45;
+        var heightStr = height.toString().concat("px");
+        mybutton.style.bottom=heightStr;
+
+    }
+    else
+    {
+      mybutton.style.bottom= "5%";
+    }
+
   }
   else
   {
     mybutton.style.display= 'none' ;
   }
 };
+
+
+function goToHome() {
+  $('html, body').animate({
+    scrollTop: 0
+
+}, 1000);
+}
 
 function goToContact() {
   $('html, body').animate({
@@ -51,7 +73,6 @@ $('.navbar-collapse').collapse('hide');
 function goToProjects() {
   $('html, body').animate({
     scrollTop: $("#projects").offset().top-50
-
 }, 1500);
 $('.navbar-collapse').collapse('hide');
 }
@@ -60,9 +81,3 @@ $('.navbar-collapse').collapse('hide');
 $('.carousel').carousel({
   interval: 2000
 })
-
-
-$('.navbar-nav>li>a').on('click', function(){
-  console.log("Test");
-    $('.navbar-collapse').collapse('hide');
-});
