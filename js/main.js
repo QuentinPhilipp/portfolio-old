43f860e89dba70571b755fa457009d13898c8141
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-08-13 18:07:17 +0200

    New sections About, skills and education

--- js/main.js
+++ js/main.js
@@ -3,38 +3,71 @@ var isInViewport = function (elem) {
     return ( bounding.top <= document.documentElement.clientHeight);
 };
 
-var offsetTop = 70;
+var offsetTop = 55;
 
 var mybutton = document.getElementById("btnscroll");
 var myfooter = document.getElementById("footer");
 
-$(window).scroll(testScroll);
+$(function() {
+   $('.scroll-down').click (function() {
+     $('html, body').animate({scrollTop: $('#about').offset().top -offsetTop }, 1000);
+     return false;
+   });
+ });
 
-window.onload = testScroll()
 
-function testScroll() {
-  var tempScrollTop = $(window).scrollTop();
-  // console.log(myfooter);
-  if(tempScrollTop>500)
-  {
-    mybutton.style.display= 'block' ;
 
-    if (isInViewport(myfooter)) {
-        var bounding = myfooter.getBoundingClientRect();
-        var height = document.documentElement.clientHeight - bounding.top+45;
-        var heightStr = height.toString().concat("px");
-        mybutton.style.bottom=heightStr;
-    }
-    else
-    {
-      mybutton.style.bottom= "5%";
-    }
-  }
-  else
-  {
-    mybutton.style.display= 'none' ;
-  }
-};
+
+ var $fade =  $(".movechart"); //Calling the class in HTML
+
+ $(window).scroll(function () { //Using the scroll global variable
+     $fade.each(function () {
+
+         fadeMiddle = $(this).offset().top + (0.4 *$(this).height());
+         windowBottom = $(window).scrollTop() + $(window).height();
+
+         if (fadeMiddle < windowBottom) {
+           $(".python").addClass("FadeInPython");
+           $(".cpp").addClass("FadeInCPP");
+           $(".html").addClass("FadeInHtml");
+           $(".angular").addClass("FadeInAngular");
+           $(".android").addClass("FadeInAndroid");
+
+         }
+     });
+ });
+
+ /* On Load: Trigger Scroll Once*/
+ $(window).scroll();
+
+
+
+ jQuery(document).ready(function($) {
+
+/*----------------------------------------------------*/
+/* FitText Settings
+------------------------------------------------------ */
+
+    setTimeout(function() {
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	 }, 100);
+
+
+   /*----------------------------------------------------*/
+   /*	Make sure that #header-background-image height is
+   /* equal to the browser height.
+   ------------------------------------------------------ */
+
+    $('header').css({ 'height': $(window).height() });
+
+    $(window).on('resize', function() {
+
+         $('header').css({ 'height': $(window).height() });
+         $('body').css({ 'width': $(window).width() })
+    });
+
+
+});
 
 
 function goToHome() {
